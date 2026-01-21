477f07eaf5947476cd9aa3d64527290d1f11b304
HLOverflow <hloverflow0@gmail.com>
2026-01-21 18:11:12 +0800

    locally debugged API authentication

--- src/index.ts
+++ src/index.ts
@@ -54,8 +54,10 @@ const CLIENT_API_KEY = process.env.CLIENT_API_KEY;
 // Middleware function to check the API key
 function checkApiKey(req, res, next) {
     // API key can be sent in query parameter 'api_key' or header 'x-api-key'
-    const apiKey = req.headers['X-CLIENT-AUTH'];
-    if (apiKey && apiKey === CLIENT_API_KEY) {
+    const apiKey = req.get('X-CLIENT-AUTH');
+    if (! apiKey){
+        res.status(403).send('Forbidden: Missing API Key');
+    } else if(apiKey === CLIENT_API_KEY) {
         // Key is valid, proceed to the next middleware or route handler
         next();
     } else {
